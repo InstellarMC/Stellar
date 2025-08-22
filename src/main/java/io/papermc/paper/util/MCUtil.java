8703c652e5debb815dc9cd060f5a97ca757d2caf
Sayakie <sayakie@kakao.com>
2025-08-22 18:46:37 +0900

    Fix save/load NaN Entity Motion
    See PaperMC/Paper#12269
    
    chery-picked from https://github.com/PaperMC/Paper/commit/5acfdd6af4b4da901a32dc845b881985028fdce2

--- src/main/java/io/papermc/paper/util/MCUtil.java
+++ src/main/java/io/papermc/paper/util/MCUtil.java
@@ -65,6 +65,18 @@ public final class MCUtil {
         return list;
     }
 
+    public static double sanitizeNanInf(final double value, final double defaultValue) {
+        return Double.isNaN(value) || Double.isInfinite(value) ? defaultValue : value;
+    }
+
+    public static Vec3 sanitizeNanInf(final Vec3 vec3, final double defaultValue) {
+        return new Vec3(
+                sanitizeNanInf(vec3.x, defaultValue),
+                sanitizeNanInf(vec3.y, defaultValue),
+                sanitizeNanInf(vec3.z, defaultValue)
+        );
+    }
+
     public static <T> CompletableFuture<T> ensureMain(CompletableFuture<T> future) {
         return future.thenApplyAsync(r -> r, MAIN_EXECUTOR);
     }
