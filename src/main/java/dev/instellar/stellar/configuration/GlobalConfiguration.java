bb8c9f07d06fb850f42edf4e4347a303137d7497
Sayakie <sayakie@kakao.com>
2025-08-22 19:48:08 +0900

    Configurable map update interval

--- src/main/java/dev/instellar/stellar/configuration/GlobalConfiguration.java
+++ src/main/java/dev/instellar/stellar/configuration/GlobalConfiguration.java
@@ -1,7 +1,11 @@
 package dev.instellar.stellar.configuration;
 
+import com.mohistmc.org.spongepowered.configurate.objectmapping.meta.Comment;
 import com.mojang.logging.LogUtils;
 import io.papermc.paper.configuration.ConfigurationPart;
+import io.papermc.paper.configuration.NestedSetting;
+import io.papermc.paper.configuration.constraint.Constraints;
+import io.papermc.paper.configuration.type.number.IntOr;
 import org.slf4j.Logger;
 
 public final class GlobalConfiguration extends ConfigurationPart {
@@ -23,6 +27,15 @@ public final class GlobalConfiguration extends ConfigurationPart {
 
         public boolean useAsync = false;
 
+    }
+
+    public Items items;
+    public class Items extends ConfigurationPart {
+
+        @Comment("Configurates interval in ticks at which the Map item update task runs.")
+        @Constraints.Min(1)
+        @NestedSetting({"map", "update-interval"})
+        public IntOr.Default mapUpdateInterval = IntOr.Default.USE_DEFAULT;
     }
 
     public Performance performance;
