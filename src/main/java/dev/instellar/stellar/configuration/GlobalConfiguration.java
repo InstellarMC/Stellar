f42a05ad662b65a31a55da18ae487dc2d89485ce
Sayakie <sayakie@kakao.com>
2025-09-17 10:06:01 +0900

    Faster sequencing of futures for chunk structure gen
    Replace `thenApply` with `thenCompose`. Once one task is completed then the next task starts immediately,
    to prevent blocking threads while waiting to complete all tasks. But may cause the sequence of future compose disorder.

--- src/main/java/dev/instellar/stellar/configuration/GlobalConfiguration.java
+++ src/main/java/dev/instellar/stellar/configuration/GlobalConfiguration.java
@@ -104,6 +104,7 @@ public final class GlobalConfiguration extends ConfigurationPart {
         public boolean useVT4UserAuthentication = false;
         @Setting("use-vt-for-chat-executor")
         public boolean useVT4ChatExecutor = false;
+        public boolean turboStructureGeneratingSequence = false;
     }
 
     public Compatibility compatibility;
