32a2565fea1c9719e42217ece7ba8c686c0bc8f5
Sayakie <sayakie@kakao.com>
2026-06-12 09:40:36 +0900

    Lazy initialization of IBlockDataList
    Test in 1 player, large render dinstance it lowered heap dump size from 928MB to 872MB

--- src/main/java/ca/spottedleaf/moonrise/patches/block_counting/BlockCountingChunkSection.java
+++ src/main/java/ca/spottedleaf/moonrise/patches/block_counting/BlockCountingChunkSection.java
@@ -4,6 +4,8 @@ import ca.spottedleaf.moonrise.common.list.IBlockDataList;
 
 public interface BlockCountingChunkSection {
 
+    public boolean anyTickingBlocks(); // Stellar: PaperMC/Paper#11500 - Lazy initialization of IBlockDataList
+
     public int moonrise$getSpecialCollidingBlocks();
 
     public IBlockDataList moonrise$getTickingBlockList();
